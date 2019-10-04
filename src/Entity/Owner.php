60b5c11bc026dae08709a6d772c207b0399d85e1
Paul Mabileau <paulmabileau@hotmail.fr>
2019-10-04 21:54:18 +0200

    Implemented __toString()s
    Created short textual representations for objects of the Region, Room and Owner classes in order to make the generated edit menus function properly.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/Entity/Owner.php
+++ src/Entity/Owner.php
@@ -6,129 +6,121 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\OwnerRepository")
  */
-class Owner
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $firstname;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $lastname;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $address;
-
-    /**
-     * @ORM\Column(type="string", length=2)
-     */
-    private $country;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Room", mappedBy="owner")
-     */
-    private $rooms;
-
-    public function __construct()
-    {
-        $this->rooms = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(?string $firstname): self
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): self
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(?string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): self
-    {
-        $this->country = $country;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Room[]
-     */
-    public function getRooms(): Collection
-    {
-        return $this->rooms;
-    }
-
-    public function addRoom(Room $room): self
-    {
-        if (!$this->rooms->contains($room)) {
-            $this->rooms[] = $room;
-            $room->setOwner($this);
-        }
-
-        return $this;
-    }
-
-    public function removeRoom(Room $room): self
-    {
-        if ($this->rooms->contains($room)) {
-            $this->rooms->removeElement($room);
-            // set the owning side to null (unless already changed)
-            if ($room->getOwner() === $this) {
-                $room->setOwner(null);
-            }
-        }
-
-        return $this;
-    }
+class Owner {
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	private $firstname;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $lastname;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	private $address;
+	/**
+	 * @ORM\Column(type="string", length=2)
+	 */
+	private $country;
+	/**
+	 * @ORM\OneToMany(targetEntity="App\Entity\Room", mappedBy="owner")
+	 */
+	private $rooms;
+	
+	public function __construct() {
+		$this->rooms = new ArrayCollection();
+	}
+	
+	public function getId(): ?int {
+		return $this->id;
+	}
+	
+	public function getFirstname(): ?string {
+		return $this->firstname;
+	}
+	
+	public function setFirstname(?string $firstname): self {
+		$this->firstname = $firstname;
+		
+		return $this;
+	}
+	
+	public function getLastname(): ?string {
+		return $this->lastname;
+	}
+	
+	public function setLastname(string $lastname): self {
+		$this->lastname = $lastname;
+		
+		return $this;
+	}
+	
+	public function getAddress(): ?string {
+		return $this->address;
+	}
+	
+	public function setAddress(?string $address): self {
+		$this->address = $address;
+		
+		return $this;
+	}
+	
+	public function getCountry(): ?string {
+		return $this->country;
+	}
+	
+	public function setCountry(string $country): self {
+		$this->country = $country;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return Collection|Room[]
+	 */
+	public function getRooms(): Collection {
+		return $this->rooms;
+	}
+	
+	public function addRoom(Room $room): self {
+		if (!$this->rooms->contains($room)) {
+			$this->rooms[] = $room;
+			$room->setOwner($this);
+		}
+		
+		return $this;
+	}
+	
+	public function removeRoom(Room $room): self {
+		if ($this->rooms->contains($room)) {
+			$this->rooms->removeElement($room);
+			// set the owning side to null (unless already changed)
+			if ($room->getOwner() === $this) {
+				$room->setOwner(null);
+			}
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Makes a textual representation of the Owner object containing its
+	 * id, firstname, lastname, address and country code.
+	 *
+	 * @return String: The String representing the object.
+	 */
+	public function __toString(): String {
+		return "$this->id: $this->firstname $this->lastname @ $this->address $this->country";
+	}
 }
--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -6,176 +6,159 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RoomRepository")
  */
-class Room
-{
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $summary;
-
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $description;
-
-    /**
-     * @ORM\Column(type="smallint")
-     */
-    private $capacity;
-
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $area;
-
-    /**
-     * @ORM\Column(type="float")
-     */
-    private $price;
-
-    /**
-     * @ORM\Column(type="string", length=400)
-     */
-    private $address;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Owner", inversedBy="rooms")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $owner;
-
-    /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Region", inversedBy="rooms")
-     */
-    private $regions;
-
-    public function __construct()
-    {
-        $this->regions = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getSummary(): ?string
-    {
-        return $this->summary;
-    }
-
-    public function setSummary(?string $summary): self
-    {
-        $this->summary = $summary;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getCapacity(): ?int
-    {
-        return $this->capacity;
-    }
-
-    public function setCapacity(int $capacity): self
-    {
-        $this->capacity = $capacity;
-
-        return $this;
-    }
-
-    public function getArea(): ?float
-    {
-        return $this->area;
-    }
-
-    public function setArea(float $area): self
-    {
-        $this->area = $area;
-
-        return $this;
-    }
-
-    public function getPrice(): ?float
-    {
-        return $this->price;
-    }
-
-    public function setPrice(float $price): self
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    public function getOwner(): ?Owner
-    {
-        return $this->owner;
-    }
-
-    public function setOwner(?Owner $owner): self
-    {
-        $this->owner = $owner;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Region[]
-     */
-    public function getRegions(): Collection
-    {
-        return $this->regions;
-    }
-
-    public function addRegion(Region $region): self
-    {
-        if (!$this->regions->contains($region)) {
-            $this->regions[] = $region;
-        }
-
-        return $this;
-    }
-
-    public function removeRegion(Region $region): self
-    {
-        if ($this->regions->contains($region)) {
-            $this->regions->removeElement($region);
-        }
-
-        return $this;
-    }
+class Room {
+	/**
+	 * @ORM\Id()
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	private $summary;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	private $description;
+	/**
+	 * @ORM\Column(type="smallint")
+	 */
+	private $capacity;
+	/**
+	 * @ORM\Column(type="float")
+	 */
+	private $area;
+	/**
+	 * @ORM\Column(type="float")
+	 */
+	private $price;
+	/**
+	 * @ORM\Column(type="string", length=400)
+	 */
+	private $address;
+	/**
+	 * @ORM\ManyToOne(targetEntity="App\Entity\Owner", inversedBy="rooms")
+	 * @ORM\JoinColumn(nullable=false)
+	 */
+	private $owner;
+	/**
+	 * @ORM\ManyToMany(targetEntity="App\Entity\Region", inversedBy="rooms")
+	 */
+	private $regions;
+	
+	public function __construct() {
+		$this->regions = new ArrayCollection();
+	}
+	
+	public function getId(): ?int {
+		return $this->id;
+	}
+	
+	public function getSummary(): ?string {
+		return $this->summary;
+	}
+	
+	public function setSummary(?string $summary): self {
+		$this->summary = $summary;
+		
+		return $this;
+	}
+	
+	public function getDescription(): ?string {
+		return $this->description;
+	}
+	
+	public function setDescription(?string $description): self {
+		$this->description = $description;
+		
+		return $this;
+	}
+	
+	public function getCapacity(): ?int {
+		return $this->capacity;
+	}
+	
+	public function setCapacity(int $capacity): self {
+		$this->capacity = $capacity;
+		
+		return $this;
+	}
+	
+	public function getArea(): ?float {
+		return $this->area;
+	}
+	
+	public function setArea(float $area): self {
+		$this->area = $area;
+		
+		return $this;
+	}
+	
+	public function getPrice(): ?float {
+		return $this->price;
+	}
+	
+	public function setPrice(float $price): self {
+		$this->price = $price;
+		
+		return $this;
+	}
+	
+	public function getAddress(): ?string {
+		return $this->address;
+	}
+	
+	public function setAddress(string $address): self {
+		$this->address = $address;
+		
+		return $this;
+	}
+	
+	public function getOwner(): ?Owner {
+		return $this->owner;
+	}
+	
+	public function setOwner(?Owner $owner): self {
+		$this->owner = $owner;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return Collection|Region[]
+	 */
+	public function getRegions(): Collection {
+		return $this->regions;
+	}
+	
+	public function addRegion(Region $region): self {
+		if (!$this->regions->contains($region)) {
+			$this->regions[] = $region;
+		}
+		
+		return $this;
+	}
+	
+	public function removeRegion(Region $region): self {
+		if ($this->regions->contains($region)) {
+			$this->regions->removeElement($region);
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Makes a textual representation of the Room object containing its
+	 * id, capacity in people, price in euros and address.
+	 *
+	 * @return String: The String representing the object.
+	 */
+	public function __toString(): String {
+		return "$this->id: $this->capacity p for $this->price € @ $this->address";
+	}
 }
