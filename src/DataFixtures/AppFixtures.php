eceaa4af0b5a40de394b5a7eb71d310a5b38480c
Paul Mabileau <paulmabileau@hotmail.fr>
2019-11-14 17:08:30 +0100

    Renamed fixtures variables
    Changed the local variables' names to be more explicit and specific to each created entity in order to make other entities easily.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,49 +16,49 @@ class AppFixtures extends Fixture {
 	public const IDF_REGION_REFERENCE = 'idf-region';
 	
 	public function load(ObjectManager $manager) {
-		$region = new Region();
-		$region->setCountry("FR");
-		$region->setName("Île-de-France");
-		$region->setPresentation("La région française capitale.");
-		$manager->persist($region);
+		$idfRegion = new Region();
+		$idfRegion->setCountry("FR");
+		$idfRegion->setName("Île-de-France");
+		$idfRegion->setPresentation("La région française capitale.");
+		$manager->persist($idfRegion);
 		
 		$manager->flush();
 		// Une fois l'instance de Region sauvée en base de données,
 		// elle dispose d'un identifiant généré par Doctrine, et peut
 		// donc être sauvegardée comme future référence.
-		$this->addReference(self::IDF_REGION_REFERENCE, $region);
+		$this->addReference(self::IDF_REGION_REFERENCE, $idfRegion);
 		
-		$owner = new Owner();
-		$owner->setFirstName("Jean-Michel");
-		$owner->setLastName("Fermier");
-		$owner->setCountry("FR");
-		$owner->setAddress("3 hameau de Bouzole");
-		$manager->persist($owner);
+		$jeanMichelOwner = new Owner();
+		$jeanMichelOwner->setFirstName("Jean-Michel");
+		$jeanMichelOwner->setLastName("Fermier");
+		$jeanMichelOwner->setCountry("FR");
+		$jeanMichelOwner->setAddress("3 hameau de Bouzole");
+		$manager->persist($jeanMichelOwner);
 		
-		$room = new Room();
-		$room->setSummary("Beau poulailler ancien à Évry");
-		$room->setDescription("Très joli espace sur paille");
-		$room->setCapacity(15);
-		$room->setArea(5.0);
-		$room->setPrice(10.0);
-		$room->setAddress("4 hameau de Bouzole");
-		$room->setOwner($owner);
+		$jmRoom1 = new Room();
+		$jmRoom1->setSummary("Beau poulailler ancien à Évry");
+		$jmRoom1->setDescription("Très joli espace sur paille");
+		$jmRoom1->setCapacity(15);
+		$jmRoom1->setArea(5.0);
+		$jmRoom1->setPrice(10.0);
+		$jmRoom1->setAddress("4 hameau de Bouzole");
+		$jmRoom1->setOwner($jeanMichelOwner);
 		// $room->addRegion($region);
-		$room->addRegion($this->getReference(self::IDF_REGION_REFERENCE));
-		$manager->persist($room);
+		$jmRoom1->addRegion($this->getReference(self::IDF_REGION_REFERENCE));
+		$manager->persist($jmRoom1);
 		
-		$client = new Client();
-		$client->setFirstName("Geoffroy");
-		$client->setLastName("Zardi");
-		$client->setEmail("geoffroy.zardi@telecom-sudparis.eu");
-		$manager->persist($client);
+		$geoffroyZardiClient = new Client();
+		$geoffroyZardiClient->setFirstName("Geoffroy");
+		$geoffroyZardiClient->setLastName("Zardi");
+		$geoffroyZardiClient->setEmail("geoffroy.zardi@telecom-sudparis.eu");
+		$manager->persist($geoffroyZardiClient);
 		
-		$comment = new Comment();
-		$comment->setText("Un certain charme rustique.");
-		$comment->setGrade(3);
-		$comment->setRoom($room);
-		$comment->setClient($client);
-		$manager->persist($comment);
+		$gzJmRoom1Comment1 = new Comment();
+		$gzJmRoom1Comment1->setText("Un certain charme rustique.");
+		$gzJmRoom1Comment1->setGrade(3);
+		$gzJmRoom1Comment1->setRoom($jmRoom1);
+		$gzJmRoom1Comment1->setClient($geoffroyZardiClient);
+		$manager->persist($gzJmRoom1Comment1);
 		
 		$manager->flush();
 	}
