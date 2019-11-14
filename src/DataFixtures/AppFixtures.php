475f2341db67351058d8476d0ed112b291f7ca3e
Paul Mabileau <paulmabileau@hotmail.fr>
2019-11-14 18:13:49 +0100

    Created new fixtures
    Added a few new entities in the fixtures to have a better test bed, especially for upcoming reservations.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,6 +9,7 @@ use App\Entity\Room;
 use App\Entity\Owner;
 use App\Entity\Client;
 use App\Entity\Comment;
+use App\Entity\Reservation;
 
 
 class AppFixtures extends Fixture {
@@ -28,6 +29,7 @@ class AppFixtures extends Fixture {
 		// donc être sauvegardée comme future référence.
 		$this->addReference(self::IDF_REGION_REFERENCE, $idfRegion);
 		
+		
 		$jeanMichelOwner = (new Owner())
 								->setFirstName("Jean-Michel")
 								->setLastName("Fermier")
@@ -37,15 +39,26 @@ class AppFixtures extends Fixture {
 		
 		$jmRoom1 = (new Room())
 						->setSummary("Beau poulailler ancien à Évry")
-						->setDescription("Très joli espace sur paille")
+						->setDescription("Très joli espace sur paille.")
 						->setCapacity(15)
 						->setArea(5.0)
 						->setPrice(10.0)
 						->setAddress("4 hameau de Bouzole")
 						->setOwner($jeanMichelOwner)
-		// $room->addRegion($region);
 						->addRegion($this->getReference(self::IDF_REGION_REFERENCE));
 		$manager->persist($jmRoom1);
+		
+		$jmRoom2 = (new Room())
+						->setSummary("Belle chambre spacieuse à Évry")
+						->setDescription("Vue sur poulailler ancien.")
+						->setCapacity(4)
+						->setArea(40.0)
+						->setPrice(70.0)
+						->setAddress("6 hameau de Bouzole")
+						->setOwner($jeanMichelOwner)
+						->addRegion($this->getReference(self::IDF_REGION_REFERENCE));
+		$manager->persist($jmRoom2);
+		
 		
 		$geoffroyZardiClient = (new Client())
 									->setFirstName("Geoffroy")
@@ -62,6 +75,29 @@ class AppFixtures extends Fixture {
 									->setRoom($jmRoom1)
 									->setClient($geoffroyZardiClient);
 		$manager->persist($gzJmRoom1Comment1);
+		
+		$gzJmRoom1Reservation = (new Reservation())
+										->setStartDate((new \DateTime())->setDate(2019, 11, 28))
+										->setEndDate((new \DateTime())->setDate(2019, 12, 02))
+										->setClient($geoffroyZardiClient)
+										->addRoom($jmRoom1);
+		$manager->persist($gzJmRoom1Reservation);
+		
+		
+		$alexisLeGlaunecClient = (new Client())
+										->setFirstName("Alexis")
+										->setLastName("Le Glaunec")
+										->setEmail("alexis.leglaunec@telecom-sudparis.eu");
+		$manager->persist($alexisLeGlaunecClient);
+		
+		$algJmRoom1Reservation = (new Reservation())
+										->setStartDate((new \DateTime())->setDate(2019, 11, 17))
+										->setEndDate((new \DateTime())->setDate(2019, 11, 23))
+										->setClient($alexisLeGlaunecClient)
+										->addRoom($jmRoom1)
+										->addRoom($jmRoom2);
+		$manager->persist($algJmRoom1Reservation);
+		
 		
 		$manager->flush();
 	}
