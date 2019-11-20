c9079320c16a6222b714b5e47561d5001244a196
Paul Mabileau <paulmabileau@hotmail.fr>
2019-11-20 19:49:42 +0100

    Customized and reworked search form
    Customized the search header form and modified the form type so that input data can be correctly communicated over the POST method and received by the form backend.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/Form/AdvancedSearchType.php
+++ src/Form/AdvancedSearchType.php
@@ -5,27 +5,19 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
-use Symfony\Component\Form\Extension\Core\Type\RangeType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 
 class AdvancedSearchType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder->add('room', SearchType::class, ['required' => false])
 				->add('region', SearchType::class, ['required' => false])
-				->add('startDate', DateType::class, ['required' => false])
-				->add('endDate', DateType::class, ['required' => false])
-				->add('priceInterval', RangeType::class, [
-						'required' => false,
-						'attr' => [
-								'min' => 20,
-								'max' => 200
-						]
-				])
-				->add('submit', SubmitType::class);
+				->add('startDate', TextType::class, ['required' => false])
+				->add('endDate', TextType::class, ['required' => false])
+				->add('minPrice', HiddenType::class, ['required' => false])
+				->add('maxPrice', HiddenType::class, ['required' => false]);
 	}
 	
 	public function configureOptions(OptionsResolver $resolver) {
