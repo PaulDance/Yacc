a5174f499834b594d12825b6b82d2086aa0e9614
Paul Mabileau <paulmabileau@hotmail.fr>
2019-11-25 19:50:20 +0100

    Added home page background images
    The expectedly unique HomePage entry holds multiple image assets from which a random one is displayed on the home page.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\HomePage;
 
 
 class HomePageController extends AbstractController {
@@ -12,6 +13,9 @@ class HomePageController extends AbstractController {
 	 */
 	public function index() {
 		return $this->render('home_page/index.html.twig',
-							['controller_name' => 'HomePageController',]);
+							['controller_name' => 'HomePageController',
+								'homePage' => $this->getDoctrine()
+													->getRepository(HomePage::class)
+													->getHomePage() ]);
 	}
 }
