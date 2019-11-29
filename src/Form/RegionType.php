a53b6aa0cec2e76a4f7e8e63a99b7c5279f8b245
Paul Mabileau <paulmabileau@hotmail.fr>
2019-11-29 13:42:27 +0100

    Fixed Room and Region forms
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/Form/RegionType.php
+++ src/Form/RegionType.php
@@ -6,23 +6,20 @@ use App\Entity\Region;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 
-class RegionType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name')
-            ->add('presentation')
-            ->add('country')
-            ->add('rooms')
-        ;
-    }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Region::class,
-        ]);
-    }
+class RegionType extends AbstractType {
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder->add('name', TextType::class, ['required' => true])
+				->add('presentation', TextareaType::class, ['required' => true])
+				->add('country', CountryType::class, ['required' => true])
+				->add('rooms');
+	}
+	
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults(['data_class' => Region::class,]);
+	}
 }
--- src/Form/RoomType.php
+++ src/Form/RoomType.php
@@ -6,27 +6,23 @@ use App\Entity\Room;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
-class RoomType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('summary')
-            ->add('description')
-            ->add('capacity')
-            ->add('area')
-            ->add('price')
-            ->add('address')
-            ->add('owner')
-            ->add('regions')
-        ;
-    }
+class RoomType extends AbstractType{
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder->add('summary', TextType::class, ['required' => true])
+				->add('description', TextareaType::class, ['required' => true])
+				->add('capacity', NumberType::class, ['required' => true])
+				->add('area', NumberType::class, ['required' => true])
+				->add('price', NumberType::class, ['required' => true])
+				->add('address', TextType::class, ['required' => true])
+				->add('owner', null, ['required' => true])
+				->add('regions', null, ['required' => true]);
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Room::class,
-        ]);
-    }
+	public function configureOptions(OptionsResolver $resolver){
+		$resolver->setDefaults(['data_class' => Room::class,]);
+	}
 }
